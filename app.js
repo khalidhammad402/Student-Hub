fa8c95d90b44b6bf24803d6171acb2b5214ef9b4
HAMMAD KHALID <hammadkhalid@HAMMADs-MacBook-Air.local>
2021-12-12 13:32:15 +0530

    Initial Commit with signup/sigin and home

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+var i = [true, false];
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
+app.post("/signup", function(req, res) {
+    res.render("signup");
+});
+
+app.get("/signin", function(req, res) {
+    res.render("signin");
+});
+
+app.post("/signin", function(req, res) {
+    res.render("signin");
+});
+
+// $(".login").on("click", function() {
+//     $("#signup_form").addClass("hidden");
+// });
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000")
+});
